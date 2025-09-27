78156ee735e72cd0353fe6a26b6c3451f6f8fb04
abhay-gupta-199 <abhaygupta1915@gmail.com>
2025-09-27 19:39:13 +0530

    Add 2nd problem

---
+++ 25-09-27/LC-215/Solution.java
@@ -0,0 +1,25 @@
+
+import java.util.*;
+
+class Solution {
+
+    public static void main(String[] args) {
+        int[] nums = {3, 2, 1, 5, 6, 4};
+        int k = 2;
+        System.out.println(findKthLargest(nums, k));
+    }
+
+    public static int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i = 0; i < k; i++) {
+            pq.offer(nums[i]);
+        }
+        for (int i = k; i < nums.length; i++) {
+            if (nums[i] > pq.peek()) {
+                pq.poll();
+                pq.offer(nums[i]);
+            }
+        }
+        return pq.peek();
+    }
+}
